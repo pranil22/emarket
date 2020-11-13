41af3d752615aa1a1be79aa8fdb8d14e39ac68b3
Pranil <pranilbhalerao99@gmail.com>
2020-11-13 22:31:24 +0530

    authentication added

--- app/src/main/java/com/example/yeskart/LoginActivity.java
+++ app/src/main/java/com/example/yeskart/LoginActivity.java
@@ -1,24 +1,85 @@
 package com.example.yeskart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText emailEditText, passwordEditText;
     private Button loginButton;
+    private FirebaseFirestore db;
+    private ProgressDialog loadingBar;
 
-    public void createAccount() {
-        Log.i("Email", emailEditText.getText().toString());
-        Log.i("Password", passwordEditText.getText().toString());
 
+    public void login() {
+        db = FirebaseFirestore.getInstance();
+
+        String email, password;
+        email = emailEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+
+
+        if(TextUtils.isEmpty(email)) {
+            Toast.makeText(LoginActivity.this, "Please enter email", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
+
+        }
+        else {
+            loadingBar.setTitle("Logging you");
+            loadingBar.setMessage("Please wait...");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            db.collection("users")
+                    .whereEqualTo("email", email)
+                    .whereEqualTo("password", password)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            boolean isCorrect = false;
+                            for (QueryDocumentSnapshot document: task.getResult()) {
+                                if(document.exists()) {
+                                    Log.i("Msg", "Success");
+                                    isCorrect = true;
+
+                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+
+                            if(!isCorrect) {
+                                Toast.makeText(LoginActivity.this, "Invalid Credientials", Toast.LENGTH_SHORT).show();
+                            }
+
+                            loadingBar.dismiss();
+
+                        }
+                    });
+        }
 
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +89,15 @@ public class LoginActivity extends AppCompatActivity {
         emailEditText = findViewById(R.id.login_email);
         passwordEditText = findViewById(R.id.login_password);
         loginButton = findViewById(R.id.login_button);
+        loadingBar = new ProgressDialog(this);
 
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createAccount();
+                login();
             }
         });
     }
+
 }
--- app/src/main/java/com/example/yeskart/RegisterActivity.java
+++ app/src/main/java/com/example/yeskart/RegisterActivity.java
@@ -14,27 +14,37 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText nameEditText, emailEditText, passwordEditText;
+    EditText nameEditText, emailEditText, passwordEditText, moblieNoEditText;
     Button registerButton;
-    public ProgressDialog loading;
+    private ProgressDialog loading;
+    private FirebaseFirestore db;
 
     public void createAccount() {
-        String email,name,password;
+        String email,name,password, mobileNo;
 
         email = emailEditText.getText().toString();
         name = nameEditText.getText().toString();
         password = passwordEditText.getText().toString();
+        mobileNo = moblieNoEditText.getText().toString();
+
 
         if(TextUtils.isEmpty(email)) {
             Toast.makeText(RegisterActivity.this, "Please enter email", Toast.LENGTH_SHORT).show();
@@ -45,79 +55,102 @@ public class RegisterActivity extends AppCompatActivity {
         else if(TextUtils.isEmpty(password)) {
             Toast.makeText(RegisterActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
         }
+        else if(TextUtils.isEmpty(mobileNo)) {
+            Toast.makeText(RegisterActivity.this, "Please enter mobileNo", Toast.LENGTH_SHORT).show();
+        }
         else {
-            loading.setTitle("Create User");
+            loading.setTitle("Creating User");
             loading.setMessage("Please wait...");
             loading.setCanceledOnTouchOutside(false);
             loading.show();
 
-            validate(name, email, password);
+            validate(name, email, password, mobileNo);
         }
     }
 
-    public void validate(final String name, final String email, final String password) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference rootRef = database.getReference();
+    public void validate(final String name, final String email, final String password, final String mobileNo) {
+        db = FirebaseFirestore.getInstance();
 
-        rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                HashMap<String, Object> userData = new HashMap<>();
-                userData.put("email", email);
-                userData.put("password", password);
-                userData.put("name", name);
-
-                rootRef.child("users").updateChildren(userData).addOnCompleteListener(new OnCompleteListener<Void>() {
+        db.collection("users")
+                .whereEqualTo("email", email)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(RegisterActivity.this, "Successfully registered", Toast.LENGTH_SHORT);
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            boolean isPresent = false;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if(document.exists()) {
+                                    isPresent = true;
+                                }
+                                Log.d("Message", document.getId() + " => " + document.getData());
+                            }
 
-                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                            startActivity(intent);
+                            if (!isPresent) {
+                                addUser(name, mobileNo, email, password);
+                            }
+                            else {
+                                Toast.makeText(RegisterActivity.this, "User already present with email", Toast.LENGTH_SHORT).show();
+                            }
                         }
                         else {
-                            Toast.makeText(RegisterActivity.this, "Something went wrong", Toast.LENGTH_SHORT);
+                            Toast.makeText(RegisterActivity.this, "Somethong went wrong", Toast.LENGTH_SHORT).show();
                         }
+                        loading.dismiss();
                     }
                 });
 //
-//                if(!(snapshot.child("users").child(email).exists())) {
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first", "Ada");
+//        user.put("last", "Lovelace");
+//        user.put("born", 1815);
 //
-//                    HashMap<String, Object> userData = new HashMap<>();
-//                    userData.put("email", email);
-//                    userData.put("password", password);
-//                    userData.put("name", name);
-//
-//                    rootRef.child("users").child(email).updateChildren(userData).addOnCompleteListener(new OnCompleteListener<Void>() {
-//                        @Override
-//                        public void onComplete(@NonNull Task<Void> task) {
-//                            if(task.isSuccessful()){
-//                                Toast.makeText(RegisterActivity.this, "Successfully registered", Toast.LENGTH_SHORT);
-//
-//                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-//                                startActivity(intent);
-//                            }
-//                            else {
-//                                Toast.makeText(RegisterActivity.this, "Something went wrong", Toast.LENGTH_SHORT);
-//                            }
-//                        }
-//                    });
-//
-//
-//                }
-//                else {
-//                    Toast.makeText(RegisterActivity.this, "Email already exists", Toast.LENGTH_SHORT);
-//                    loading.dismiss();
-//                }
-            }
+//        db.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d("tag", "DocumentSnapshot added with ID: " + documentReference.getId());
+//                        loading.dismiss();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w("tag", "Error adding document", e);
+//                        loading.dismiss();
+//                    }
+//                });
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
 
-            }
-        });
+    }
+
+    public void addUser(String name, String mobileNo, String email, String password) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", name);
+        user.put("mobileNo", mobileNo);
+        user.put("email", email);
+        user.put("password", password);
+
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(RegisterActivity.this, "Succesfully added user", Toast.LENGTH_SHORT).show();
+                        Log.d("Msg", "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                        Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("tag", "Error adding document", e);
+                    }
+                });
     }
 
     @Override
@@ -128,8 +161,10 @@ public class RegisterActivity extends AppCompatActivity {
         nameEditText = findViewById(R.id.register_full_name);
         emailEditText = findViewById(R.id.register_email);
         passwordEditText = findViewById(R.id.register_password);
+        moblieNoEditText = findViewById(R.id.register_mobile_no);
         registerButton = findViewById(R.id.register_button);
         loading = new ProgressDialog(this);
+
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
